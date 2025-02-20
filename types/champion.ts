8991a94fd3a1c45cdb740b26a07c6288f29c6efa
OmoingShiur <shiursmurfwildrift@gmail.com>
2025-02-20 18:00:25 +0000

    Delete types directory

--- types/champion.ts
+++
@@ -1,9 +0,0 @@
-export interface Champion {
-  id: string
-  name: string
-  class: string
-  image: string
-}
-
-export type ChampionClass = "Fighter" | "Tank" | "Assassin" | "Mage" | "Support" | "Marksman"
-
